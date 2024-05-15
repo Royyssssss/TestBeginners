100bba5cd4760d1598d254ec8faf8dd81ff0efe4
Royyssssss <137406143+Royyssssss@users.noreply.github.com>
2024-05-15 15:58:25 +0900

    Create bgsq1byroy.py

---
+++ bgsq1byroy.py
@@ -0,0 +1,42 @@
+question 1
+a)
+number1 = (30)
+
+sum = number1 * 60
+print( sum, "second" )
+b)
+number1 = (5)
+
+sum = number1 * 60
+print( sum, "second" )
+
+question 2
+a)
+number1 = (7)
+
+sum = number1 * 2
+print("Jane receives at least", sum, "cards." )
+b)
+number1 = (59)
+
+sum = number1 * 2
+print("Jane receives at least", sum, "cards." )
+
+question 3
+a)
+year = (65)
+
+sum = year * 365
+print("you are", sum, "days old." )
+b)
+year = (20)
+
+sum = year * 365
+print("you are", sum, "days old." )
+
+question 3.1 
+year = (65)
+
+sum = year * 365
+sum2 = 36500 - sum
+print("you are", sum, "days old.", "In", sum2, "days you will be 100 years old!" )
